ffda40ccff84f348bc805b6855e5b7990c99a2ca
Rogério Carvalho Schneider <stockrt@gmail.com>
2010-10-29 15:47:24 -0200

    build with less and more generic config options

--- build.sh
+++ build.sh
@@ -4,33 +4,15 @@ TAG="$1"
 NGINX_VERSION="$2"
 PREFIX="nginx-push-stream-module"
 
-CONFIGURE_OPTIONS="--without-select_module \
---without-poll_module \
---without-http_charset_module \
---without-http_ssi_module \
---without-http_auth_basic_module \
---without-http_autoindex_module \
---without-http_geo_module \
---without-http_map_module \
---without-http_referer_module \
---without-http_fastcgi_module \
---without-http_memcached_module \
---without-http_limit_zone_module \
---without-http_limit_req_module \
---without-http_empty_gif_module \
---without-http_browser_module \
---without-http_upstream_ip_hash_module \
---without-mail_pop3_module \
---without-mail_imap_module \
---without-mail_smtp_module \
---with-http_stub_status_module \
---add-module=nginx-push-stream-module"
-
 if [[ -z "$TAG" || -z "$NGINX_VERSION" ]]
 then
     echo "Usage: $0 <tag> <nginx_version>"
     exit 1
 fi
+
+CONFIGURE_OPTIONS="\
+--with-http_stub_status_module \
+--add-module=nginx-push-stream-module"
 
 (./pack.sh $TAG && \
 cd build && \
