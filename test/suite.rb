74ba7eb29b9482971fcfa534b629c248f43be2d5
Rogério Carvalho Schneider <stockrt@gmail.com>
2011-04-17 01:42:23 -0300

    suite indent

--- test/suite.rb
+++ test/suite.rb
@@ -4,6 +4,6 @@
 require 'test/unit'
 
 Dir.glob('test_*.rb').each do|f|
-    test_case = File.expand_path(f, File.dirname(__FILE__)).gsub('.rb', '')
-    require test_case
+  test_case = File.expand_path(f, File.dirname(__FILE__)).gsub('.rb', '')
+  require test_case
 end
