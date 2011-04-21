d6bb1b6e5714facd8603c40ae069a98b7190ae59
Wandenberg Peixoto <wandenberg@gmail.com>
2011-04-21 14:21:18 -0300

    replace suite.rb for the task tests on Rakefile

--- test/suite.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# coding: utf-8
-
-require 'test/unit'
-
-Dir.glob('test_*.rb').each do|f|
-  test_case = File.expand_path(f, File.dirname(__FILE__)).gsub('.rb', '')
-  require test_case
-end
